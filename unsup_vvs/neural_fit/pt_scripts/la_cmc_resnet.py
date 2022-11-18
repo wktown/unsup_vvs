34dddcdc2ac8c2a43f93f2822ccaf323a320a494
wktown <112629266+wktown@users.noreply.github.com>
2022-11-18 12:06:49 -0500

    Update la_cmc_resnet.py
    fix for "No module names torchvision.models.utils"

--- unsup_vvs/neural_fit/pt_scripts/la_cmc_resnet.py
+++ unsup_vvs/neural_fit/pt_scripts/la_cmc_resnet.py
@@ -5,7 +5,7 @@ https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 """
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
